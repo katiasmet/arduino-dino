7a5d9fc8beb86c974bed3be402bb90e5c8ba13d8
Alex Bernard <itsalexbernard@gmail.com>
2016-11-09 16:27:51 +0100

    added keycodes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,39 @@
 init = () => {
 
-  console.log('hello world');
+  window.addEventListener('keydown',(e)=>{
+    if (e.keyCode === 40) {
+      makeError();
+    }
 
+    if (e.keyCode === 38) {
+      success1();
+    }
 
+    if (e.keyCode === 39) {
+      success2();
+    }
 
+    if (e.keyCode === 37) {
+      success3();
+    }
+  });
+
+}
+
+makeError = () =>{
+  console.log('ERROR');
+}
+
+success1 = () =>{
+  console.log('eerste is goed');
+}
+
+success2 = () =>{
+  console.log('tweede is goed');
+}
+
+success3 = () =>{
+  console.log('derde is goed');
 }
 
 init();
